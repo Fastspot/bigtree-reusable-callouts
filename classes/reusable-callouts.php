47076b4d6062fe91a85dbed438a5e4aed1c61309
Tim Buckingham <mr@timbuckingham.com>
2016-08-18 15:42:48 -0400

    Adding a report and archive option.

--- classes/reusable-callouts.php
+++ classes/reusable-callouts.php
@@ -5,6 +5,8 @@
 	*/
 
 	class BTXReusableCallouts extends BigTreeModule {
+		
+		private static $CalloutCache = array();
 
 		var $Table = "btx_reusable_callouts";
 
@@ -28,8 +30,8 @@
 			$reused = $mod->get($callout["__reusable_callout_id"]);
 
 			// Using a deleted one, just hope there's some other data I guess
-			if (empty($reused)) {
-				return $callout;
+			if (empty($reused) || $reused["archived"]) {
+				return false;
 			}
 
 			// Data is actually an array containing one callout - done this way for ease of code re-use
@@ -37,6 +39,218 @@
 			$callout["type"] = $reused["type"];
 
 			return $callout;
+		}
+		
+		/*
+		    Function: getUsage
+				Returns an array of module entries and pages in which the callout is used.
+		
+			Parameters:
+				callout - A reusable callout or resuable callout ID
+		
+			Returns:
+				An array of arrays.
+		*/
+		
+		static function getUsage($callout) {
+			$callout_id = is_int($callout) ? $callout : $callout["id"];
+			$settings = array();
+			
+			return array(
+				"pages" => static::getUsageInPages($callout_id),
+				"modules" => static::getUsageInModules($callout_id),
+				"settings" => static::getUsageInSettings($callout_id)
+			);
+		}
+		
+		static function getUsageInPages($callout_id) {
+			$pages = array();
+			$templates = array();
+			
+			// We're going to look in every page that has a template containing callouts
+			$template_query = array();
+			$q = sqlquery("SELECT * FROM bigtree_templates WHERE resources LIKE '%callouts%' OR resources LIKE '%matrix%'");
+			
+			while ($template = sqlfetch($q)) {
+				$resources = json_decode($template["resources"], true);
+				
+				foreach ($resources as $resource) {
+					if (!isset($templates[$template["id"]]) && ($resource["type"] == "callouts" || $resource["type"] == "matrix")) {
+						$templates[$template["id"]] = $resources;
+						$template_query[] = "template = '".$template["id"]."'";
+						break;
+					}
+				}
+			}
+			
+			$q = sqlquery("SELECT * FROM bigtree_pages WHERE (".implode(" OR ",$template_query).")");
+			
+			while ($page = sqlfetch($q)) {
+				$resources = json_decode($page["resources"], true);
+				
+				foreach ($templates[$page["template"]] as $resource) {
+					$found = false;
+					
+					if ($resource["type"] == "callouts") {
+						foreach ($resources[$resource["id"]] as $callout) {
+							$found = static::parseCalloutData($callout, $callout_id);
+							
+							if ($found) {
+								break;
+							}
+						}
+					} elseif ($resource["type"] == "matrix") {
+						$found = static::parseMatrix($resource["options"]["columns"], $resource[$resource["id"]], $callout_id);
+					}
+					
+					if ($found) {
+						$pages[] = array(
+							"page" => $page["id"],
+							"title" => $page["nav_title"],
+							"field" => $resource["title"]
+						);
+					}
+				}
+			}
+			
+			return $pages;
+		}
+		
+		static function getUsageInModules($callout_id) {
+			$modules = array();
+			$form_query = sqlquery("SELECT * FROM bigtree_module_forms WHERE fields LIKE '%callouts%' OR fields LIKE '%matrix%'");
+			
+			while ($form = sqlfetch($form_query)) {
+				$fields = json_decode($form["fields"], true);
+				
+				// Now get the contents of the table
+				$q = sqlquery("SELECT * FROM `".$form["table"]."`");
+
+				while ($entry = sqlfetch($q)) {
+					foreach ($fields as $field) {
+						$found = false;
+						
+						if ($field["type"] == "matrix") {
+							$data = json_decode($entry[$field["column"]], true);
+							$found = static::parseMatrix($field["options"]["columns"], $data, $callout_id);
+						} elseif ($field["type"] == "callouts") {
+							$data = json_decode($entry[$field["column"]], true);
+
+							foreach (array_filter((array) $data) as $callout_entry) {
+								$found = static::parseCalloutData($callout_entry, $callout_id);
+								
+								if ($found) {
+									break;
+								}
+							}
+						}
+						
+						if ($found) {
+							$modules[] = array(
+								"module" => $form["module"],
+								"form" => $form["id"],
+								"title" => $form["title"],
+								"entry" => $entry["id"],
+								"field" => $field["title"]
+							);
+						}
+					}
+				}
+			}
+			
+			return $modules;
+		}
+		
+		static function getUsageInSettings($callout_id) {
+			$settings = array();
+			$q = sqlquery("SELECT id, options, name, type FROM bigtree_settings WHERE type = 'matrix' OR type = 'callouts'");
+			
+			while ($setting = sqlfetch($q)) {
+				$found = false;
+				$value = BigTreeCMS::getSetting($setting["id"]);
+				
+				if ($setting["type"] == "matrix") {
+					$options = json_decode($setting["options"], true);
+					$found = static::parseMatrix($options["columns"], $value, $callout_id);
+				} else {
+					foreach ($value as $callout_data) {
+						$found = static::parseCalloutData($callout_data, $callout_id);
+						
+						if ($found) {
+							break;
+						}
+					}
+				}
+				
+				if ($found) {
+					$settings[] = array(
+						"id" => $setting["id"],
+						"title" => $setting["name"]
+					);
+				}
+			}
+			
+			return $settings;
+		}
+		
+		static function parseMatrix($matrix_columns, $matrix_data, $callout_id) {
+			foreach (array_filter((array) $matrix_data) as $entry) {
+				foreach ($matrix_columns as $matrix_field) {
+					if ($matrix_field["type"] == "matrix") {
+						$found = static::parseMatrix($matrix_field["options"]["columns"], $entry[$matrix_field["id"]], $callout_id);
+						
+						if ($found) {
+							return true;
+						}
+					} elseif ($matrix_field["type"] == "callouts") {
+						foreach ($entry[$matrix_field["id"]] as $callout_entry) {
+							$found = static::parseCalloutData($callout_entry, $callout_id);
+							
+							if ($found) {
+								return true;
+							}
+						}
+					}
+				}
+			}
+			
+			return false;
+		}
+		
+		static function parseCalloutData($callout_data, $callout_id) {
+			// This is the reusable callout
+			if (!empty($callout_data["__reusable_callout_id"]) && $callout_data["__reusable_callout_id"] == $callout_id) {
+				return true;
+			}
+			
+			// This callout isn't the one we're looking for but it may contain another callouts or matrix field
+			$type = $callout_data["type"];
+			
+			if (!isset(static::$CalloutCache[$type])) {
+				static::$CalloutCache[$type] = BigTreeAdmin::getCallout($type);
+			}
+			
+			$callout_fields = static::$CalloutCache[$type]["resources"];
+			
+			foreach ($callout_fields as $field) {
+				if ($field["type"] == "callouts") {
+					foreach ($callout_data[$field["id"]] as $another_callout) {
+						$found = static::parseCalloutData($another_callout, $callout_id);
+						
+						if ($found) {
+							return true;
+						}
+					}
+				} elseif ($field["type"] == "matrix") {
+					$found = static::parseMatrix($field["options"]["columns"], $callout_data[$field["id"]], $callout_id);
+					
+					if ($found) {
+						return true;
+					}
+				}
+			}
+			
+			return false;
 		}
 
 		static function publishHook($table, $id, $changes, $many_to_many, $tags) {
--- field-types/callout-list/draw.php
+++ field-types/callout-list/draw.php
@@ -3,13 +3,19 @@
 	// $_POST is something we have control over both inline and when calling the AJAX resources
 	if (!isset($_POST["btx_reusable_callouts_editor"])) {
 		$module = new BTXReusableCallouts;
-		$available = $module->getMatching("type", $bigtree["callout"]["id"]);
-	
+		$available = $module->getMatching(array("archived", "type"), array("", $bigtree["callout"]["id"]));
+		$existing_value = false;
+
 		if (count($available)) {
 			$field["options"]["list"] = array();
-	
+
 			foreach ($available as $item) {
 				$field["options"]["list"][] = array("value" => $item["id"], "description" => $item["title"]);
+
+				// We need to make sure the current value is in the list since callout types can switch
+				if ($item["id"] == $field["value"]) {
+					$existing_value = $field["value"];
+				}
 			}
 ?>
 <fieldset>
@@ -26,21 +32,21 @@
 
 		var Form;
 		var Fieldset;
-		var LastValue = "<?=$field["value"]?>";
+		var LastValue = "<?=$existing_value?>";
 		var OtherFieldsets;
 		var OtherFields;
 		var RequiredFields;
 
 		// Add a ready hook to populate our values
 		BigTree.ReadyHooks.push(function() {
-			Form = $("#<?=$field["id"]?>").parents("form");
+			Form = $("#<?=$field["id"]?>").parents(".callout_fields");
 			Fieldset = $("#<?=$field["id"]?>").parents("fieldset");
 			OtherFieldsets = Form.find("fieldset").not(Fieldset);
-			OtherFields = OtherFieldsets.find("input, select, textarea");
 			RequiredFields = OtherFieldsets.find("input.required, select.required, textarea.required, input.numeric, input.email, input.link");
+			OtherFields = OtherFieldsets.find("input, select, textarea");
 		});
 
-		<?php if ($field["value"]) { ?>
+		<?php if ($existing_value) { ?>
 		// Add a ready hook to disable other form elements if this is enabled
 		BigTree.ReadyHooks.push(saveInputStates);
 		<?php } ?>
